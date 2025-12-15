89c6b61a76633487dc942d858b0d66f55d0218f2
Pralin Khaira <be.coder.19@gmail.com>
2025-12-15 23:08:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count Indices to Balance Even and Odd Sums/count-indices-to-balance-even-and-odd-sums.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int cntWays(vector<int>& arr) {
+        int n = arr.size();
+        int totalEven = 0, totalOdd = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (i & 1) totalOdd += arr[i];
+            else totalEven += arr[i];
+        }
+
+        int prefEven = 0, prefOdd = 0;
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (i & 1) totalOdd -= arr[i];
+            else totalEven -= arr[i];
+
+            int newEven = prefEven + totalOdd;
+            int newOdd  = prefOdd  + totalEven;
+
+            if (newEven == newOdd) ans++;
+
+            if (i & 1) prefOdd += arr[i];
+            else prefEven += arr[i];
+        }
+
+        return ans;
+    }
+};
